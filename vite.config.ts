19435fe6818b5661b16e544c5c2a0fa43419906f
Toby Ryuk <ryuk.toby@gmail.com>
2023-10-22 09:55:30 +0200

    Set base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
 export default defineConfig({
+  base: "/studio-seating-randomizer/",
   plugins: [react()],
   resolve: {
     alias: {
